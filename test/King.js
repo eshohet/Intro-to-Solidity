37de0e9eadf1a27aaf5482124c2141a5bd5e6446
Elliot Shohet <elliot.shohet@gmail.com>
2018-04-03 10:57:43 -0700

    Add semicolon

--- test/King.js
+++ test/King.js
@@ -11,5 +11,5 @@ contract('King', async function ([deployer, userOne, userTwo, userThree]) {
     //Let's start off by ensuring that the user sends ether
     it("If ether is not sent, reject the transaction.", async() => {
         const txn = await king.takeThrone();
-    })
+    });
 });
