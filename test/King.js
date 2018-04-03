c71b399691e27f660b2c94b93cc0524a4c1d2104
Elliot Shohet <elliot.shohet@gmail.com>
2018-04-03 10:56:05 -0700

    Typo

--- test/King.js
+++ test/King.js
@@ -8,7 +8,7 @@ contract('King', async function ([deployer, userOne, userTwo, userThree]) {
         king = await King.new({from: deployer});
     });
 
-    //Let's start off by ensureing that the user sends ether
+    //Let's start off by ensuring that the user sends ether
     it("If ether is not sent, reject the transaction.", async() => {
         const txn = await king.takeThrone();
     })
